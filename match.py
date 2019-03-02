18c77d5684a74d4d2524c593fbb949e7e52f6652
thiagop-usp <thiagop@usp.br>
2019-03-02 18:18:42 -0300

    matching working great, for some reason normed correlation was the only functional method tho

---
+++ match.py
@@ -0,0 +1,30 @@
+import cv2
+import numpy as np
+import matplotlib as plt
+
+#reads both the full image and the template thats going to be looked for
+#0 means grayscale
+frnds = cv2.imread('friends.jpg', 0)
+tmpt = cv2.imread('chandler.jpg', 0)
+
+#finding resolutions
+imgh, imgw = frnds.shape
+tmph, tmpw = tmpt.shape
+
+#matches the full image with the template
+match = cv2.matchTemplate(frnds, tmpt, 3);
+minv, maxv, minl, maxl = cv2.minMaxLoc(match)
+
+#finding edges of the match
+top_left = maxl
+bottom_right = (maxl[0] + tmpw, maxl[1] + tmph)
+
+#drawing rectangle on the match to see if it's right (255 = white, 2 = thickness)
+cv2.rectangle(frnds, top_left, bottom_right, 255, 2)
+
+#showing result
+cv2.imshow('image', frnds)
+key = cv2.waitKey(0)
+#waits for ESC key to be pressed to leave
+if key == 27:
+    cv2.destroyAllWindows()
